9000aa0812651e4224c7e278f09e0f2f84da3602
github username <github email address>
2020-05-20 15:03:26 -0500

    added auto login

--- app/controllers/auth_controller.rb
+++ app/controllers/auth_controller.rb
@@ -11,10 +11,20 @@ class AuthController < ApplicationController
       render json: { message: 'Invalid username or password' }, status: :unauthorized
     end
   end
+
+  def auto_login
+    if current_user
+      token = auth_header.split(' ')[1]
+      render json: { user: UserSerializer.new(@user), jwt: token }, status: :accepted
+    else
+      render json: {errors: "No user logged in"}
+    end
+  end
+
  
   private
  
   def user_login_params
-    params.require(:user).permit(:username, :password)
+    params.require(:user).permit(:username, :password, :token)
   end
 end

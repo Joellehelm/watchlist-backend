873fb5b99ef058d1358c139c368ac0502fa0256d
joellehelm <joellenhelm@gmail.com>
2020-06-02 09:49:09 -0500

    heroku env var

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::API
    
    
     def encode_token(payload)
-      JWT.encode(payload, ENV["SKEY"])
+      JWT.encode(payload, ENV[‘SECRET_KEY_BASE’])
     end
    
     def auth_header
@@ -14,7 +14,7 @@ class ApplicationController < ActionController::API
       if auth_header
         token = auth_header.split(' ')[1]
         begin
-          JWT.decode(token, ENV["SKEY"], true, algorithm: 'HS256')
+          JWT.decode(token, ENV[‘SECRET_KEY_BASE’], true, algorithm: 'HS256')
         rescue JWT::DecodeError
           nil
         end

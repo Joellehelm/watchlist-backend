ee76878a0c69b9d16b1fa5ead5f2b524c47d751c
joellehelm <joellenhelm@gmail.com>
2020-08-26 11:38:25 -0500

    added usershow validation

--- app/models/user_show.rb
+++ app/models/user_show.rb
@@ -1,4 +1,6 @@
 class UserShow < ApplicationRecord
     belongs_to :show
     belongs_to :user
+    validates :show, uniqueness: { scope: :user,
+    message: "A show can only be added to a watchlist once." }
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,25 +1,28 @@
 
 
-joelle = User.create(username: "joelle", email: "joelle@joelle.com", password_digest: "j")
-ashley = User.create(username: "ashley", email: "ashley@ashley.com", password_digest: "a")
+# joelle = User.create(username: "joelle", email: "joelle@joelle.com", password_digest: "j")
+# ashley = User.create(username: "ashley", email: "ashley@ashley.com", password_digest: "a")
 
-office = Show.create(name: "The Office", genre: "Comedy", poster: "im a picture", total_seasons: "12")
-
-
-
-season1 = Season.create(season_number: 1, show_id: 1)
-
-episode1 = Episode.create(name: "first", season_id: 1)
-
-episode2 = Episode.create(name: "second", season_id: 1)
-
-episode3 = Episode.create(name: "third", season_id: 1)
-
-
-progress1 = Progress.create(user_id: 1, show_id: 1, season_id: 1, episode_id: 2)
-
-
-friend = Friendship.create(user_id: 1, friend_id: 2)
+# office = Show.create(name: "The Office", genre: "Comedy", poster: "im a picture", total_seasons: "12")
 
 
 
+# season1 = Season.create(season_number: 1, show_id: 1)
+
+# episode1 = Episode.create(name: "first", season_id: 1)
+
+# episode2 = Episode.create(name: "second", season_id: 1)
+
+# episode3 = Episode.create(name: "third", season_id: 1)
+
+
+# progress1 = Progress.create(user_id: 1, show_id: 1, season_id: 1, episode_id: 2)
+
+
+# friend = Friendship.create(user_id: 1, friend_id: 2)
+
+
+
+s = Show.first
+u = User.first
+us = UserShow.create(show: s, user: u)
